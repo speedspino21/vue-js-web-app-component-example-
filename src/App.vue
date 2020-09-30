74ca15aa4ad7cf7c0dfcea1cd7399f40e82c58fb
Yanming Zhang <yzhan27@outlook.com>
2020-09-29 22:48:35 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id = "app">
+        <Header title="VueJs" />
+        <Profile :user="user" />
+    </div>
+</template>
+
+<script>
+  import Header from './components/Header';
+  import Profile from './components/Profile';
+
+  export default {
+      name: 'App',
+      components: {
+        Header,
+        Profile
+      },
+      data: function () {
+        return {
+                  user: {
+                    name: "James",
+                    image: require("./assets/spider-man.jpg")
+                  }
+               }
+        }
+   }
+    
+</script>
+
+<style>
+    body {
+      background: #F3F3F3;
+      margin: 0;
+    }
+
+    #app {
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;      
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="../assets/logo.png" alt="VueJs Logo" />
+        </div>
+        <div class="title">
+            <h1>{{ title }}</h1>
+        </div>
+        <div class="filler"></div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    props: {
+        title: String
+    }
+}
+</script>
+<style scoped>
+    header {
+        width: 100%;
+        height: 60px;
+        background: #171717;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .logo {
+        flex: 1;
+    }
+    .logo img {
+        height: 30px;
+    }
+    .title {
+        flex: 3;
+    }
+    .title h1 {
+        color: #f3F3F3;
+        font-size: 28px;
+        line-height: 60px;
+        font-weight: 300;
+    }
+    .filler {
+        flex: 1;
+    }
+</style>
